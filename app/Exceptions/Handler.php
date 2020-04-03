367c40d22690091f6fe9de7f5a4fd14bb2673194
David <david@aimtech.am>
2020-04-03 17:26:19 +0400

    unauthenticated massage change

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -51,5 +52,20 @@ class Handler extends ExceptionHandler
     public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            if ($request->is('api/*')) {
+                return response()->json(
+                    $data = array(
+                        'data' => array(),
+                        'success' => false,
+                        'msg' => 'Unauthenticated',
+                    ), 401);
+            }
+        }
+        return redirect()->guest(route('login'));
     }
 }
