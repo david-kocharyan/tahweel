7d6dbc04807d429a0dd428bc8ffa72edeb652775
David <david@aimtech.am>
2020-04-17 15:15:47 +0400

    file upload resize

--- app/helpers/FileUploadHelper.php
+++ app/helpers/FileUploadHelper.php
@@ -2,20 +2,37 @@
 
 
 namespace App\helpers;
+
 use Illuminate\Http\File;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class FileUploadHelper
 {
-    public static function upload($data, array $allowedfileExtension, string $folder)
+    public static function upload($data, array $allowedfileExtension, string $folder, bool $resize = false, int $width = 800, int $height = 800)
     {
         $images = [];
         foreach ($data as $image) {
-            if(in_array("*", $allowedfileExtension) || in_array($image->getClientOriginalExtension(), $allowedfileExtension)) {
-                $image = Storage::putFile($folder, new File($image), 'public');
+            if (in_array("*", $allowedfileExtension) || in_array($image->getClientOriginalExtension(), $allowedfileExtension)) {
+                if ($resize) {
+                    $image = self::resize($image, $folder, $width, $height);
+                } else {
+                    $image = Storage::putFile($folder, new File($image), 'public');
+                }
                 $images[]["image"] = $image;
             }
         }
         return $images;
+    }
+
+    private static function resize($image, string $folder, int $width, int $height)
+    {
+        $img = Image::make($image);
+        $fileName = uniqid("", true) . '.' . $image->getClientOriginalExtension();
+        $img->resize($width, $height, function ($constraint) {
+            $constraint->aspectRatio();
+        })->stream();
+        Storage::put($folder . '/' . $fileName, $img, 'public');
+        return $folder . "/" . $fileName;
     }
 }
