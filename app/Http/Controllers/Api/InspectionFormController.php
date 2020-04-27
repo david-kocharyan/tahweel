e763b52f7120562d073adb0213361c9227bdb834
unknown <aghabekyan.pargev@gmail.com>
2020-04-27 12:17:54 +0400

    added inspection form

--- app/Http/Controllers/Api/InspectionFormController.php
+++ app/Http/Controllers/Api/InspectionFormController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers\Api;
 use App\helpers\FileUploadHelper;
 use App\Http\Controllers\Controller;
 use App\Model\InspectionForm;
+use App\Model\Phase;
 use Illuminate\Http\Request;
 use App\helpers\ResponseHelper;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 
 class InspectionFormController extends Controller
 {
+
+    private $base_url;
+
+    public function __construct()
+    {
+        $this->base_url = URL::to('/');
+    }
+
     public function index(Request $request)
     {
         $validator = Validator::make($request->all(),
@@ -43,6 +54,7 @@ class InspectionFormController extends Controller
         }
 
         $image = FileUploadHelper::upload($request->signature, ['*'], "");
+        DB::beginTransaction();
 
         $form = new InspectionForm();
         $form->inspection_id = $request->inspection_id;
@@ -69,6 +81,49 @@ class InspectionFormController extends Controller
 
         $form->save();
 
+        $currentPhase = Phase::where("inspection_id", $request->inspection_id)->orderBy("id", "DESC")->first();
+        $phase = new Phase();
+        $phase->phase = ($request->approved == InspectionForm::DECLINED) ? ($currentPhase->phase ?? 1) : ( ($request->warranty == InspectionForm::NO_WARRANTY) ? 2 : $currentPhase->phase );
+        $phase->status = ($request->approved == InspectionForm::DECLINED) ? (Phase::REJECTED) : ( ($request->warranty == InspectionForm::NO_WARRANTY) ? Phase::NEW : Phase::COMPLETED );
+        $phase->save();
+        DB::commit();
         return ResponseHelper::success(array());
+    }
+
+    public function getForm(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+            [
+                'inspection' => 'required|integer',
+            ]);
+
+        if ($validator->fails()) {
+            return ResponseHelper::fail($validator->errors()->first(), ResponseHelper::UNPROCESSABLE_ENTITY_EXPLAINED);
+        }
+
+        $form = InspectionForm::where("inspection_id", $request->inspection)
+            ->selectRaw("
+                            inspection_id,
+                            pre_plaster,
+                            before_tiles_installer,
+                            final_after_finishing,
+                            bathrooms_inspected,
+                            kitchen_inspected,
+                            service_counters_inspected,
+                            bathroom_other_tahweel_materials,
+                            bathroom_other_technical_issue,
+                            roof_other_tahweel_materials,
+                            roof_other_tahweel_valve,
+                            roof_other_technical_issue,
+                            manifold_other_tahweel_materials,
+                            manifold_other_tahweel_valve,
+                            manifold_sunlight,
+                            manifold_insulated,
+                            $this->base_url || '/uploads/' || signature,
+                            approved,
+                            reason,
+                            warranty")->orderBy("id", "DESC")->first();
+        $resp = array("form" => $form);
+        return ResponseHelper::success($resp);
     }
 }
--- app/Model/InspectionForm.php
+++ app/Model/InspectionForm.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class InspectionForm extends Model
 {
-    //
+
+    const NO_WARRANTY = 0;
+    const NORMAL_WARRANTY = 1;
+    const FULL_WARRANTY = 2;
+
+    const DECLINED = 0;
+    const APPROVED = 1;
 }
