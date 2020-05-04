5f17b9f75f6b3952877069d4d657580e589d85f1
unknown <aghabekyan.pargev@gmail.com>
2020-05-04 15:25:17 +0400

    latestRepeate

--- app/Model/Inspection.php
+++ app/Model/Inspection.php
@@ -44,7 +44,7 @@ class Inspection extends Model
 
     public function latestRepeated()
     {
-        return $this->hasMany("App\Model\Phase", "inspection_id", "id")->where("status", Phase::REPEATED)->orderBy("id", "DESC")->first();
+        return $this->hasOne("App\Model\Phase", "inspection_id", "id")->where("status", Phase::REPEATED)->orderBy("id", "DESC");
     }
 
 }
