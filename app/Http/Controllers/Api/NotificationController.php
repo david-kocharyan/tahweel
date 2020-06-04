bda5ba28eff74be942450286ab16b63a9b4703b5
Pargev <aghabekyan.pargev@gmail.com>
2020-06-04 17:28:37 +0400

    added get notifications

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -14,7 +14,7 @@ class NotificationController extends Controller
     {
         $limit = !is_numeric($request->limit) ? 20 : $request->limit;
         $notifications = Notification::selectRaw("id, type, title, body, (extract(EPOCH from created_at) * 1000) as date")->where(["user_id" => Auth::guard('api')->user()->id, "active" => 1])->orderBy("id", "DESC")->paginate($limit);
-        $notifications = Notification::selectRaw("id, type, title, body, (extract(EPOCH from created_at) * 1000) as date")->get();
+        $notifications = Notification::selectRaw("id, type, title, body, (extract(EPOCH from created_at) * 1000) as date")->paginate($limit);
         return ResponseHelper::success($notifications, true);
     }
 }
