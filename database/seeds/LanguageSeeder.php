b22aa77af30e20eea90806871d0acf1039132c1b
David <david@aimtech.am>
2020-07-03 13:43:22 +0400

    urdu language add

--- database/seeds/LanguageSeeder.php
+++ database/seeds/LanguageSeeder.php
@@ -14,6 +14,7 @@ class LanguageSeeder extends Seeder
         \App\Model\Language::insert([
             ["lng" => "en"],
             ["lng" => "ar"],
+            ["lng" => "ur"],
         ]);
     }
 }
