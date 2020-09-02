a87923d3c93ad524211c558f3af05c1ce5ad3e0e
David <david@aimtech.am>
2020-09-02 13:32:38 +0400

    add user for notif table fix 1

--- app/Model/Notification.php
+++ app/Model/Notification.php
@@ -11,6 +11,6 @@ class Notification extends Model
 
     public function user()
     {
-        return $this->hasOne("App\User", "user_id", "id");
+        return $this->belongsTo("App\User", "user_id", "id");
     }
 }
