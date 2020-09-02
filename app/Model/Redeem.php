a96c4ce3bf283e1db97b14b15b3b5d6eda73ee42
David <david@aimtech.am>
2020-09-02 14:07:12 +0400

    redeem table test 5

--- app/Model/Redeem.php
+++ app/Model/Redeem.php
@@ -14,6 +14,6 @@ class Redeem extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class, "user_id", "id");
+        return $this->belongsTo(User::class, "plumber_id", "id");
     }
 }
