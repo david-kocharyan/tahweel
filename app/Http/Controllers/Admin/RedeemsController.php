2785337b20990bfcd81721baf5889e8de66aeb27
David <david@aimtech.am>
2020-09-02 14:41:34 +0400

    redeem table test 12

--- app/Http/Controllers/Admin/RedeemsController.php
+++ app/Http/Controllers/Admin/RedeemsController.php
@@ -18,7 +18,7 @@ class RedeemsController extends Controller
      */
     public function index()
     {
-        $data = Redeem::selectRaw("point, (extract(EPOCH from created_at) * 1000) as redeemDate, product_id")->with(['user', "product" => function ($query) {
+        $data = Redeem::with(['user', "product" => function ($query) {
             $query->selectRaw("products.id, product_languages.name, product_languages.description, (extract(EPOCH from products.created_at) * 1000) as date, products.point")
                 ->leftJoin('product_languages', 'products.id', '=', 'product_languages.product_id')
                 ->where(array('product_languages.language_id' => 1));
