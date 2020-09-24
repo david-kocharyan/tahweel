120f22d4359e8f1f2a03b9e9f327f0e1947e1bd4
David <david@aimtech.am>
2020-09-24 11:02:59 +0400

    warranty table fix 2

--- app/Model/CastomerWarrantySave.php
+++ app/Model/CastomerWarrantySave.php
@@ -9,11 +9,11 @@ class CastomerWarrantySave extends Model
 {
     public function inspector()
     {
-        return $this->hasOne(User::class, "inspector_id", "id");
+        return $this->belongsTo(User::class, "inspector_id", "id");
     }
 
     public function customer()
     {
-        return $this->hasOne(Customer::class, "customer_id", "id");
+        return $this->belongsTo(Customer::class, "customer_id", "id");
     }
 }
