42e1266f32bbb4c8927ee1f7cd002234c81c3899
David <david@aimtech.am>
2020-10-07 16:38:40 +0400

    send notification fix 14

--- app/Http/Controllers/Admin/SendNotificationController.php
+++ app/Http/Controllers/Admin/SendNotificationController.php
@@ -31,7 +31,9 @@ class SendNotificationController extends Controller
         ]);
 
         if (count($request->role) > 1) {
-            $users = User::with('tokensForAll')->get();
+            $users = User::with(['tokensForAll' => function($query){
+                $query->whereHas('token');
+            }])->get();
         } else {
             $users = User::with('tokens')->where('role', $request->role[0])->get()->pluck('token');
         }
