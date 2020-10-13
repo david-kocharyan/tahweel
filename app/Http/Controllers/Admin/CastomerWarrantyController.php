420740cb463ed9857dec0e1611723d54794cc314
David <david@aimtech.am>
2020-10-13 17:51:26 +0400

    change name possition try 21

--- app/Http/Controllers/Admin/CastomerWarrantyController.php
+++ app/Http/Controllers/Admin/CastomerWarrantyController.php
@@ -119,8 +119,8 @@ class CastomerWarrantyController extends Controller
         $customer = Customer::where('inspection_id', $data->inspection_id)->first();
         $file = Certificate::where('type', $data->warranty_type)->first()->file;
 
-        $Arabic = new I18N_Arabic('Glyphs');
-        $text = $Arabic->utf8Glyphs($customer->full_name);
+
+        $text = $customer->full_name;
 
         $img = Image::make(public_path("uploads/$file"));
         $img->rotate(-90);
