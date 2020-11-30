b55cfa0298289ab2bebe2ed90cead6fa735127de
David <david@aimtech.am>
2020-11-30 15:28:09 +0400

    certif fix 24

--- resources/views/certificate/certificate_1.blade.php
+++ resources/views/certificate/certificate_1.blade.php
@@ -6,45 +6,70 @@
     <title>Certificate 1</title>
     <style>
         .certificate {
-            width: 100%;
-            height: 100vh;
+            width: 1000px;
+            height: 500px;
             background-image: url("{{asset('assets/certificate/1.jpg')}}");
             background-repeat: no-repeat;
             background-size: 100% 99%;
         }
 
-        .text-2, .text-3 {
+        .text-1, .text-2, .text-3 {
             position: relative;
             width: 390px;
             height: 38px;
             right: 19%;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
             font-size: 19px;
-            text-align: center;
             color: #696d70;
         }
 
+        .text-1 {
+            top: 26%;
+        }
+
         .text-2 {
-            top: 30.5%;
+            top: 29.5%;
             left: 52%;
             width: 450px;
         }
 
         .text-3 {
-            top: 31%;
+            top: 33%;
             left: 57%;
             width: 500px;
         }
 
-        .text-6, .text-7, .text-8, .text-9, .text-10, .text-11, .text-id-1, .text-id-2 {
+        .text-4, .text-5, .text-6, .text-7, .text-8, .text-9, .text-10, .text-11, .text-id-1, .text-id-2 {
             font-size: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: flex-end;
             color: #696d70;
+        }
+
+        .text-4 {
+            position: absolute;
+            width: 45px;
+            height: 35px;
+            top: 45%;
+            right: 13.7%;
+        }
+
+        .text-5 {
+            position: absolute;
+            width: 40px;
+            height: 35px;
+            top: 45%;
+            right: 20.5%;
         }
 
         .text-6 {
             position: absolute;
             width: 40px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 40.2%;
         }
 
@@ -60,7 +85,7 @@
             position: absolute;
             width: 40px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 45.35%;
         }
 
@@ -68,15 +93,15 @@
             position: absolute;
             width: 40px;
             height: 35px;
-            top: 47%;
-            right: 55%;
+            top: 45%;
+            right: 72%;
         }
 
         .text-10 {
             position: absolute;
             width: 40px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 74.6%;
         }
 
@@ -84,7 +109,7 @@
             position: absolute;
             width: 40px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 77.55%;
         }
 
@@ -92,7 +117,7 @@
             position: absolute;
             width: 170px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 59.55%;
         }
 
@@ -100,7 +125,7 @@
             position: absolute;
             width: 170px;
             height: 35px;
-            top: 47%;
+            top: 45%;
             right: 25.55%;
         }
 
@@ -123,6 +148,7 @@
         <div class="text-11">{{$phase_one->created_at->format('d')}}</div>
         <div class="text-id-1">{{$phase_one->id}}</div>
     @endif
+
 </div>
 
 </body>
