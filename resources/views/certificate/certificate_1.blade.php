4ae65a9eddc6318ce4c111a40f403a610f9a9499
David <david@aimtech.am>
2020-11-30 15:38:02 +0400

    certif fix 29

--- resources/views/certificate/certificate_1.blade.php
+++ resources/views/certificate/certificate_1.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Certificate 1</title>
     <style>
-        @page { margin: 100px; }
         .certificate {
             position: fixed;
             /*width: 100%;*/
